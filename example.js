ae1e07af10c2980d5bb4ef0e9c7d5419e8de0174
CMoncur <CMoncur@gmail.com>
2016-10-20 12:33:04 -0700

    Jigawatt stuff... more of it

--- example.js
+++ example.js
@@ -13,49 +13,23 @@ mg.connect('mongodb://localhost/data/pollar-dev')
 mg.Promise = global.Promise
 
 /* --- Jigawatt Middleware --- */
-/*
-Because indexes of our lists start at 0, responses are coming
-from the database unmatched to the option thereunto pertaining.
-We need to decrement each response so they align with the options
-that the users are voting for.
-*/
-// getAnswer :: [ Integer ] -> [ Integer ]
+// getAnswer :: [ Object ] -> [ Integer ]
 const getAnswer = (arr) => R.compose(
   R.map(R.dec)
 , R.pluck('answer')
 )(arr)
 
-/*
-Each city comes from the database with the city name and the
-country or state that the city is in.  This is more verbose than
-what we want, so let's truncate each string at the comma so that
-we're just left with the city name.
-*/
 // getCity :: Object -> [ String ]
 const getCity = (obj) => R.map(
   R.replace(/\,.*$/, '')
 , obj.questions
 )
 
-/*
-We know that the length of the IDs for each poll are equal to 24,
-so we want to ensure that the ID gathered from the endpoint is
-the correct length.
-*/
-// isCorrectLength :: String -> Bool
-const isCorrectLength = (str) => R.equals(24, str.length)
-
-/*
-We have two lists now -- a list of cities and a list of responses.
-We have to tally the votes for each city and display the total
-for each city alongside the city name.
-*/
 // tallyVotes :: [ String ] -> [ Int ] -> [ Object ]
 const tallyVotes = (options, responses) => {
   // Tally total vote for a specific city
   return R.map((str) => {
-    let ind = R.indexOf(str, options)
-
+    let ind   = R.indexOf(str, options)
     let votes = R.compose(
       R.length
     , R.filter(R.equals(ind))
@@ -66,16 +40,43 @@ const tallyVotes = (options, responses) => {
 }
 
 /*
-This Jigawatt middleware function is given an ID as input data.
-First, it ensures that the ID is only integers and lowercase
-numbers, then ensures that is is the correct length to query the
-database.
+getPollQuestion
+The input will look like this:
 
-IO makes two calls to the database, one to gather the poll and
-details, and the next to gather all responses to that poll, to
-include data about the responder.
+{ "poll" : "57f691081739bcb1144630a2"
+, "responder" : "1"
+, "response" : 2
+}
 
-The poll will have a structure like this:
+And the output will look like this:
+
+{ "title" : "What is your favorite city?"
+, "responder" : "1"
+, "response" : "Vladivostok, Russia"
+}
+*/
+const getPollQuestion = {
+  awesomize: (v) => ({
+    poll : {
+      read : R.path([ 'data', 'pollId' ])
+    , validate : [ v.required ]
+    }
+  })
+
+, io: (req, data) => ({
+    poll : db.Poll.findOne({ _id: data.poll })
+  })
+
+, transform: (req, data) => ({
+    poll        : data.poll.title
+  , responderId : data.responder
+  , response    : R.nth(R.dec(data.response), data.poll.questions)
+  })
+}
+
+/*
+getPollResults
+The poll will have an initial structure like this:
 
 { _id: 57f691081739bcb1144630a2,
   title: 'What is your favorite city?',
@@ -109,10 +110,7 @@ And the responses will be structured like this:
   ...
 ]
 
-Lastly, transform will take the data collected by IO and format
-it in a structure that we want.  In this case, we want to return
-an object with a poll title and a list of response objects that
-include cities and their respective votes:
+The output will look like this:
 
 {
 	"poll": "What is your favorite city?",
@@ -124,11 +122,12 @@ include cities and their respective votes:
              ]
 }
 */
-const pollDetails = {
+const getPollResults = {
   awesomize: (v) => ({
     pollId : {
-      sanitize : R.toLower
-    , validate : isCorrectLength
+      read     : R.path([ 'params', 'pollId' ])
+    , sanitize : [ R.toLower ]
+    , validate : [ v.required ]
     }
   })
 
@@ -146,18 +145,57 @@ const pollDetails = {
   })
 }
 
-/* --- ROUTES --- */
-app.get('/best-city-results', (req, res) => {
-  const data       = { pollId : req.params.id }
-      , formatPoll = JW(pollDetails, data)
+/*
+getSingleVote
+The vote will have an initial structure like this:
 
-  formatPoll(data, { json : (data) => data })
-    .then((details) => res.send(details))
-    .catch((err)    => console.log('There was an error here: ' + err))
-})
+{ _id: 57f69170526f05b5dbe4d035,
+  poll_id: '57f691081739bcb1144630a2',
+  user_id: '1111',
+  answer: 1,
+  private: false,
+  __v: 0 }
+
+The output will look like this:
+
+{ "poll" : "57f691081739bcb1144630a2"
+, "responder" : "1"
+, "response" : 2
+}
+*/
+
+const getSingleVote = {
+  awesomize: (v) => ({
+    voteId : {
+      read : R.path([ 'params', 'voteId' ])
+    , validate : [ v.required ]
+    }
+  })
+
+, io: (req, data) => ({
+    vote : db.Vote.findOne({ _id: data.voteId })
+  })
+, transform: (req, data) => ({
+    pollId    : data.vote.poll_id
+  , responder : data.vote.user_id
+  , response  : data.vote.answer
+  })
+}
+
+//pollId : '57f691081739bcb1144630a2'
+//voteId : '57f691784949d5b68c337548'
+
+/* --- ROUTES --- */
+// Will display poll question with options and responses
+app.get('/poll/:pollId', JW(getPollResults))
+
+// Will display a single vote with voter metadata
+app.get('/vote/:voteId', JW(
+  JW.pipe(getSingleVote, getPollQuestion)
+))
 
 /* --- SERVER --- */
-app.listen(8000, (err) => {
-  if (err) return console.log('Could not start server!')
-  console.log('Listening on port 8000')
-})
+app.listen(8000, (err) => err
+  ? console.log('Could not start server!')
+  : console.log('Listening on port 8000')
+)
